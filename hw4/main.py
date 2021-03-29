094d99882861e7772a7fc7e5b98c76ae2180de18
go-may <75523685+go-may@users.noreply.github.com>
2021-03-29 10:30:09 +0300

    Add files via upload

---
+++ hw4/main.py
@@ -0,0 +1,26 @@
+# 1
+musicians = ['Гітарист', 'Барабанщик', 'Піаніст', 'Вокаліст']
+for musician in musicians:
+    print (musician)
+
+# 2
+group_month_money = 100
+group_year_money = group_month_money * 12
+print (group_year_money)
+
+# 3
+group_name = 'Пайтон на гітарі'
+hits = ['Пайтон і джунглі', 'Прінт та вивід на консоль моїх емоцій', 'Інпут свій настрій']
+# нумерируем список
+for hit in hits:
+    print (hits.index(hit) + 1, hit)
+
+# 4
+like = 123
+likes = 0
+time = 6
+i = 0
+while i < time:
+    print (i, 'Час', likes, 'лайков')
+    likes += like
+    i += 1
